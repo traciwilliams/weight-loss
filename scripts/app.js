ca7739ff90f269607796aa82a4228eb54faf1336
Traci Williams <ug3000@gmail.com>
2017-07-03 14:50:08 -0500

    completed calculator

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,17 @@
+/*
+when a button is clicked then the display number fumction is taking the value of the button clikced
+
+
+*/
+
+
 window.onload = function() {
 	console.log("weight loss - javascript loaded")
+}
+
+function displaynumber(x) {
+	//console.log(x);
+	//console.log(calform.text1.value);
+	//calform.text1.value = x;
+	calform.text1.value = calform.text1.value + x;
 }
